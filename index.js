2a85637f06044ecaaa77b1fb0a9cf883294073d5
Marcus Cheung <myc2131@tc.columbia.edu>
2018-04-19 05:09:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson!"
+<<<<<<< HEAD
+=======
+"Hello, " + specialGuest + "!"
+>>>>>>> 4c8cf25991a8412865d8de6ff013b30adec5ed76
 
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
